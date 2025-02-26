0439f5a8a4168afac32f73cdff23e93018f980e0
init <boitchenko.marianna@gmail.com>
2025-02-26 18:23:26 +0300

    Тренировки в полёте2

--- main.py
+++ main.py
@@ -14,5 +14,14 @@ def index():
 @app.route('/training/<prof>')
 def training(prof):
     return  render_template("training.html", prof=prof.lower())
+
+
+@app.route('/list_prof/<mrk>')
+def list_prof(mrk):
+    profession = ['врач', 'строитель', 'инженер']
+    return  render_template('list_prof.html', profession=profession, mrk=mrk.lower())
+
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1', debug=True)
+
